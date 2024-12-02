e48fb47d54599fd144bf179af79fe32fac742eff
xupy <xupy@spdb.com.cn>
2024-12-02 09:22:02 +0800

    Initial Commit

---
+++ MyC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MyC
+//
+//  Created by spdbdev on 2024/12/2.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
